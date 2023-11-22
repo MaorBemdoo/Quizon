799ecd0b1a3f130dc32c99028f9dfb32d84cf56a
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-22 10:05:46 +0100

    Gave btn a link

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,13 +1,13 @@
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-
+import { Link } from 'react-router-dom'
 
 const Error = ({className}: any) => {
     return (
         <main className={className}>
             <Typography variant="h1" color="initial">404</Typography>
             <Typography variant="h4" color="initial">Looks like you missed your way</Typography>
-            <Button variant="contained" color="primary">Back to Home</Button>
+            <Link to='/'><Button variant="contained" color="primary">Back to Home</Button></Link>
         </main>
     )
 }
