b31f9b0c26b929d4399467d2ff02606c798a705b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-22 10:21:14 +0100

    Worked on page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import GlobalStyles from "./styles/GlobalStyles"
 import HomeStyle from "./styles/Home.style"
 import ErrorStyle from "./styles/ErrorStyle"
+import CategoryStyle from "./styles/Category.style"
 import SharedLayout from "./components/SharedLayout"
+import SharedCategoryLayout from "./components/SharedCategoryLayout"
 
 function App() {
 
@@ -12,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<SharedLayout/>}>
           <Route index element={<HomeStyle/>}/>
+          <Route path="category" element={<SharedCategoryLayout/>}>
+            <Route index element={<Navigate to='/'/>}/>
+            <Route path=":category" element={<CategoryStyle/>}></Route>
+          </Route>
           <Route path="*" element={<ErrorStyle />}/>
         </Route>
       </Routes>
