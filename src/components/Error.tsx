34adbe9a5d542e42fa2da20db67864de31380b1d
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-22 10:04:26 +0100

    Worked on Error Page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,14 @@
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
 
 const Error = ({className}: any) => {
     return (
-        <main className={className}>Error</main>
+        <main className={className}>
+            <Typography variant="h1" color="initial">404</Typography>
+            <Typography variant="h4" color="initial">Looks like you missed your way</Typography>
+            <Button variant="contained" color="primary">Back to Home</Button>
+        </main>
     )
 }
 
--- src/styles/ErrorStyle.tsx
+++ src/styles/ErrorStyle.tsx
@@ -2,7 +2,16 @@ import styled from "styled-components";
 import Error from "../components/Error";
 
 const ErrorStyle = styled(Error)`
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
+    text-align: center;
+    min-height: 100vh;
+    & > h1{
+        font-size: 15rem;
+    }
 `
 
 export default ErrorStyle
