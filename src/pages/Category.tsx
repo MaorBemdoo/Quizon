1300b61293f85496717f29a85a4d83932efc169c
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-19 23:20:37 +0000

    Worked on no qui

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,16 +1,16 @@
 import { useParams } from "react-router-dom"
 import CategoryError from "./Error/CategoryError"
 
-const Category = ({className, categories, setTempCategories}: any) => {
+const Category = ({className, categories}: any) => {
 
     const { categoryId }: any = useParams()
 
-    const category = categories === "error" ? "netError" : categories?.find(({ id }) => categoryId == id)
+    const category = categories === "error" ? "netError" : categories.find(({ id }) => categoryId == id )
 
     return (
         <main className={className}>
-            {category !== null && category !== "netError" ? (
-                <h1>{category?.name}</h1>
+            {category !== null && category !== undefined ? (
+                <h1>{category.name}</h1>
             ) : <CategoryError error={category}/>}
         </main>
     )
