887b844fca00b6b96c93e5cdd52af689bf8a143a
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-19 23:34:17 +0000

    Added more meta tags in helmet category

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -12,6 +12,20 @@ const Category = ({className, categories}: any) => {
         <main className={className}>
             <Helmet>
                 <title>{category.name} - Quizon</title>
+                <meta name="title" content={`${category.name} - Quizon`} />
+                {/* <meta name="description" content="" /> */}
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:title" content={`${category.name} - Quizon`} />
+                {/* <meta property="og:description" content="" /> */}
+                <meta property="og:image" content={`src/assets/category${category.id}`} />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:title" content={`${category.name} - Quizon`} />
+                {/* <meta property="twitter:description" content="" /> */}
+                <meta property="twitter:image" content={`src/assets/category${category.id}`} />
             </Helmet>
             {category !== null && category !== undefined ? (
                 <h1>{category.name}</h1>
