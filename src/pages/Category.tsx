007f5de4db6b5219f89e593c1c7f9de18d73966a
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-21 09:01:35 +0000

    Worked on loading

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -12,41 +12,52 @@ const Category = ({className}: CategoryProps) => {
 
     const { categoryId } = useParams()
     const categories = useAppSelector((state) => state.categories.categories)
-    let error = useAppSelector((state) => state.categories.error)
+    const isLoading = useAppSelector((state) => state.categories.loading)
+    let error = useAppSelector((state) => state.categories.error) || "error"
 
     if(error == "error"){
         error = "netError"
     }
 
-    const category = error == "netError" ? {} : categories?.find(({ id }) => Number(categoryId) == id )
+    const category = categories?.find(({ id }) => Number(categoryId) == id )
+
     if(typeof category === 'undefined'){
         error = "404"
     }
 
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (typeof category === 'undefined') {
+        return <CategoryError error={error} />;
+    }
+
+    if (category === null) {
+        return null; // Handle case when category is null
+    }
+
+
     return (
         <main className={className}>
-            {error === null && typeof category !== 'undefined' ? (
-                <>
-                    <Helmet>
-                        <title>{`${category.name} - Quizon`}</title>
-                        <meta name="title" content={`${category.name} - Quizon`} />
-                        {/* <meta name="description" content="" /> */}
+            <Helmet>
+                <title>{`${category.name} - Quizon`}</title>
+                <meta name="title" content={`${category.name} - Quizon`} />
+                {/* <meta name="description" content="" /> */}
 
-                        <meta property="og:type" content="website" />
-                        <meta property="og:url" content="https://metatags.io/" />
-                        <meta property="og:title" content={`${category.name} - Quizon`} />
-                        {/* <meta property="og:description" content="" /> */}
-                        <meta property="og:image" content={`src/assets/category${category.id}`} />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:title" content={`${category.name} - Quizon`} />
+                {/* <meta property="og:description" content="" /> */}
+                <meta property="og:image" content={`src/assets/category${category.id}`} />
 
-                        <meta property="twitter:card" content="summary_large_image" />
-                        <meta property="twitter:url" content="https://metatags.io/" />
-                        <meta property="twitter:title" content={`${category.name} - Quizon`} />
-                        {/* <meta property="twitter:description" content="" /> */}
-                        <meta property="twitter:image" content={`src/assets/category${category.id}`} />
-                    </Helmet>
-                    <h1>{category.name}</h1>
-                </>
-            ) : <CategoryError error={error}/>}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:title" content={`${category.name} - Quizon`} />
+                {/* <meta property="twitter:description" content="" /> */}
+                <meta property="twitter:image" content={`src/assets/category${category.id}`} />
+            </Helmet>
+            <h1>{category.name}</h1>
         </main>
     )
 }
