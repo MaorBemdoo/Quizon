1f0c5e4a4aa8f34113052ea94d871ce7f5128b8b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-21 19:56:24 +0000

    changed header padding

--- src/styles/Header.style.tsx
+++ src/styles/Header.style.tsx
@@ -5,7 +5,7 @@ const HeaderStyle = styled(Header)`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: .2em;
+    padding: 1em;
     & img{
         height: 50px;
         width: 40px;
