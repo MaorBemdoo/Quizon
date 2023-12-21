685343e1c47395afd597f5478776e07844f22f28
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-21 17:28:02 +0000

    Added Header.tsx

--- src/pages/SharedLayouts/SharedLayout.tsx
+++ src/pages/SharedLayouts/SharedLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Header from "../../components/Header"
 
 const SharedLayout = () => {
     return (
-        <Outlet/>
+        <>
+            <Header/>
+            <Outlet/>
+        </>
     )
 }
 
