ef2c9d7f33d3b5dbe98a27918ddfa0f04610dd1b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-27 23:49:09 +0100

    worked on modal html

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,6 @@
+import Typography from '@mui/material/Typography'
+import logo from '../../public/logo.png'
+
 
 interface ModalProps{
     className?: string
@@ -5,7 +8,18 @@ interface ModalProps{
 
 const Modal = ({ className }: ModalProps) => {
     return (
-        <div className={className}>Modal</div>
+        <div className={className}>
+            <img src={logo} alt="Quizon logo" />
+            <Typography variant="h4" color="initial">Setting up your quiz</Typography>
+            <div>
+                <Typography variant="body1" color="initial">While waiting just take some time to read some <b>Rules and Important notes</b></Typography>
+                <ul>
+                    <li>There are 10 questions to be anwsered</li>
+                    <li>Your result is going to be displayed at the end of the quiz</li>
+                    <li>Don't refresh the page</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
