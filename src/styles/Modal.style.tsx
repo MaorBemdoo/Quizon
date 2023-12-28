651d7886ce422b98f5c7417ea995625b75c6faab
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-12-28 01:08:25 +0100

    Still styling the modal

--- src/styles/Modal.style.tsx
+++ src/styles/Modal.style.tsx
@@ -1,8 +1,20 @@
 import styled from "styled-components";
 import Modal from "../components/Modal";
 
+const backgroundColor = () => {
+    const dark = localStorage.getItem("color-scheme")
+    return dark ? (JSON.parse(dark) ? "#121212" : 'white') : {}
+}
+
 const ModalStyle = styled(Modal)`
-    
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: grid;
+    place-items: center;
+    padding: 2em;
+    background-color: ${backgroundColor};
 `
 
 export default ModalStyle
