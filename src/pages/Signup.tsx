604438f4786a66650a4518cfee4e1fd0d2750bd2
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-01-19 22:19:52 +0100

    stated working on sigup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,181 @@
+import { Helmet } from "react-helmet-async";
+import background from "../assets/background1.jpg";
+import logo from "../../public/logo.png";
+import {
+    Alert,
+    FormControl,
+    FormHelperText,
+    Input,
+    InputLabel,
+    Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material";
 
+interface SignupProps {
+    className?: string;
+    dark: boolean;
+}
 
-const Signup = ({ className }: { className?: string }) => {
-  return <main className={className}>Signup</main>;
+const Signup = ({ className }: SignupProps) => {
+    return (
+        <main className={className}>
+            <Helmet>
+                <title>Signup - Quizon</title>
+            </Helmet>
+            <div>
+                <img src={background} alt="Background" />
+            </div>
+            <div className="signup">
+                <img src={logo} alt="Quizon logo" />
+                <Typography
+                    variant="h4"
+                    color="initial"
+                    sx={{ fontWeight: "500" }}
+                >
+                    Create Account
+                </Typography>
+                <Typography
+                    variant="body2"
+                    color="initial"
+                    sx={{ opacity: ".8" }}
+                >
+                    Please enter your details
+                </Typography>
+                <Alert
+                    severity="error"
+                    variant="filled"
+                    // sx={{ display: `${uniError ? "flex" : "none"}` }}
+                >
+                    Incorrect credentials
+                </Alert>
+                <FormControl
+                    variant="standard"
+                    sx={{ margin: "1.3em 0" }}
+                    // error={emailError || uniError}
+                    fullWidth
+                >
+                    <InputLabel
+                        htmlFor="email"
+                        sx={{ color: "initial !important" }}
+                    >
+                        Email
+                    </InputLabel>
+                    <Input
+                        id="email"
+                        aria-describedby="email-text"
+                        // value={user.email}
+                        sx={{ color: "initial" }}
+                        // onChange={(e) =>
+                        //     // setUser({ ...user, email: e.target.value })
+                        // }
+                        onFocus={() => {
+                            // setEmailError(false);
+                            // setPwdError(false);
+                            // setUniError(false);
+                        }}
+                    />
+                    {/* <FormHelperText id="fullname-text" hidden={!emailError}>
+                            {user.email.trim() == ""
+                                ? "Email field is required"
+                                : "Invalid email format: example@test.com"}
+                        </FormHelperText> */}
+                </FormControl>
+                <FormControl
+                    variant="standard"
+                    // error={pwdError || uniError}
+                    fullWidth
+                >
+                    <InputLabel
+                        htmlFor="password"
+                        sx={{ color: "initial !important" }}
+                    >
+                        Password
+                    </InputLabel>
+                    <Input
+                        id="password"
+                        aria-describedby="password-text"
+                        // type={pwdVisibility ? "text" : "password"}
+                        // value={user.password}
+                        sx={{ color: "initial" }}
+                        // onChange={(e) =>
+                        //     // setUser({ ...user, password: e.target.value })
+                        // }
+                        onFocus={() => {
+                            // setEmailError(false);
+                            // setPwdError(false);
+                            // setUniError(false);
+                        }}
+                    />
+                    {/* {!pwdVisibility ? (
+                            <VisibilityOutlined
+                                sx={{
+                                    cursor: "pointer",
+                                    position: "absolute",
+                                    right: "15px",
+                                    top: "30px",
+                                    transform: "translate(-50%, -50%)",
+                                }}
+                                onClick={() => setPwdVisibility(!pwdVisibility)}
+                            />
+                        ) : (
+                            <VisibilityOffOutlined
+                                sx={{
+                                    cursor: "pointer",
+                                    position: "absolute",
+                                    right: "15px",
+                                    top: "30px",
+                                    transform: "translate(-50%, -50%)",
+                                }}
+                                onClick={() => setPwdVisibility(!pwdVisibility)}
+                            />
+                        )} */}
+                    {/* <FormHelperText id="password-text" hidden={!pwdError}>
+                            Password field is required
+                        </FormHelperText> */}
+                </FormControl>
+                <Typography
+                    variant="body2"
+                    color="initial"
+                    className="forgot-pwd"
+                >
+                    Forgot password?
+                </Typography>
+                <button
+                    style={{
+                        fontSize: "1.2rem",
+                        fontWeight: "500",
+                        color: "white !important",
+                    }}
+                    // onClick={submitHandler}
+                >
+                    <b style={{ color: "white !important" }}>Log In</b>
+                </button>
+                <div className="or">
+                    <hr />
+                    <Typography variant="body1" color="initial">
+                        OR
+                    </Typography>
+                </div>
+                <button
+                    className="google-signup"
+                    style={{ fontSize: "1.2rem" }}
+                    // onClick={signWithGoogle}
+                >
+                    <img
+                        // src={googleLogo}
+                        alt="google Logo"
+                        width={25}
+                        height={25}
+                    />
+                    <b>Log in with Google</b>
+                </button>
+                <Typography variant="body1" color="initial">
+                    Already have an account? <Link to="/login">Login</Link>
+                </Typography>
+            </div>
+        </main>
+    );
 };
 
 export default Signup;
--- src/styles/Signup.style.tsx
+++ src/styles/Signup.style.tsx
@@ -2,7 +2,139 @@ import styled from "styled-components";
 import Signup from "../pages/Signup";
 
 const SignupStyle = styled(Signup)`
-    
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+    font-family: 'Noto Sans', sans-serif;
+    border-radius: 10px;
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    z-index: 3;
+    & > *{
+        flex-basis: 50%;
+    }
+    & *{
+        color: ${props => props.dark ? "white" : "black"};
+    }
+    & > div:first-child{
+        clip-path: polygon(0 0, 71% 0, 100% 100%, 0% 100%);
+        &  img{
+            object-fit: cover;
+            object-position: top;
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .signup{
+        position: relative;
+        text-align: center;
+        padding: 2em;
+        max-width: 400px;
+        margin: 0 auto;
+        &  *{
+            &::before,
+            &::after{
+                border-color: ${props => props.dark ? "white" : "black"} !important;
+            }
+        }
+        & > button{
+            width: 100%;
+            padding: .5em;
+            border-radius: 50px;
+            cursor: pointer;
+        }
+        .forgot-pwd{
+            position: relative;
+            left: 100%;
+            transform: translateX(-100%);
+            opacity: .4;
+            text-align: end; 
+            cursor: pointer;
+            padding: 1em 0;
+            overflow: hidden;
+            width: fit-content;
+            &:hover{
+                &::after{
+                    width: 115px;
+                    transition: all 1s ease;
+                }
+            }
+            &::after{
+                content: "";
+                position: absolute;
+                left: calc(100% - 115px);
+                bottom: 1em;
+                width: 0;
+                height: 1px;
+                background-color: ${props => props.dark ? "white" : "black"};
+            }
+        }
+        .or{
+            padding: 2em 0;
+            position: relative;
+            & hr{
+                width: 100%;
+                height: .6px;
+                background-color: ${props => props.dark ? "white" : "black"};
+            }
+            & p{
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                background-color: ${props => !props.dark ? "white" : "black"};
+            }
+        }
+        & .google-signup{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 1em;
+            background-color: #f9f3f3 !important;
+            & b{
+                color: black !important;
+            }
+            &:hover{
+                background-color: #ded8d8 !important;
+            }
+        }
+        & > *:last-child{
+            position: absolute;
+            bottom: 2em;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            & a{
+                font-weight: 500;
+                position: relative;
+                overflow: hidden;
+                &:hover{
+                    &::after{
+                        width: 100%;
+                        transition: all 1s ease;
+                    }
+                }
+                &::after{
+                    content: "";
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 0;
+                    height: 1px;
+                    background-color: ${props => props.dark ? "white" : "black"};
+                }
+            }
+        }
+    }
+    @media(max-width: 700px){
+        & > div:first-child{
+            display: none;
+            flex-basis: 0;
+        }
+        .signup{
+            width: calc(100% - 2em);
+            flex-basis: 100%;
+        }
+    }
 `
 
 export default SignupStyle
