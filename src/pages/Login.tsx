b362f99871a20c33989ff8750f35e6fee44c3b23
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-01-20 21:23:44 +0100

    Worked on email error handling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,7 +22,10 @@ const Login = ({ className }: LoginProps) => {
     })
     const [emailError, setEmailError] = useState(false)
     const [pwdError, setPwdError] = useState(false)
-    const [uniError, setUniError] = useState(false)
+    const [uniError, setUniError] = useState({
+        status: false,
+        msg: ''
+    })
 
     const [pwdVisibility, setPwdVisibility] = useState(false)
     const auth = getAuth(app);
@@ -47,11 +50,24 @@ const Login = ({ className }: LoginProps) => {
         signInWithEmailAndPassword(auth, user.email, user.password)
             .then((userCredential) => {
                 console.log(userCredential)
-                setUniError(false)
+                setUniError({
+                    status: false,
+                    msg: ''
+                })
             })
             .catch((error) => {
-                console.log(error)
-                setUniError(true)
+                console.log(error.code)
+                if(error.code == "auth/network-request-failed"){
+                    setUniError({
+                        status: true,
+                        msg: 'No internet connection'
+                    })
+                }else{
+                    setUniError({
+                        status: true,
+                        msg: 'Incorrect credentials'
+                    })
+                }
             });
     }
 
@@ -63,6 +79,17 @@ const Login = ({ className }: LoginProps) => {
             }).catch((error) => {
                 console.log(error)
                 console.log(GoogleAuthProvider.credentialFromError(error))
+                if(error.code == "auth/internal-error"){
+                    setUniError({
+                        status: true,
+                        msg: "No internet connection"
+                    })
+                }else{
+                    setUniError({
+                        status: true,
+                        msg: "An error occured"
+                    })
+                }
             });
     }
 
@@ -76,8 +103,8 @@ const Login = ({ className }: LoginProps) => {
                 <img src={logo} alt="Quizon logo" />
                 <Typography variant="h4" color="initial" sx={{fontWeight: "500"}}>Welcome back!</Typography>
                 <Typography variant="body2" color="initial" sx={{opacity: ".8"}}>Please enter your details</Typography>
-                <Alert severity="error" variant="filled" sx={{display: `${uniError ? "flex" : "none"}`}}>Incorrect credentials</Alert>
-                <FormControl variant="standard" sx={{margin: "1.3em 0"}} error={emailError || uniError} fullWidth>
+                <Alert severity="error" variant="filled" sx={{display: `${uniError.status ? "flex" : "none"}`}}>{uniError.msg}</Alert>
+                <FormControl variant="standard" sx={{margin: "1.3em 0"}} error={emailError || uniError.status} fullWidth>
                     <InputLabel htmlFor="email" sx={{color: "initial !important"}}>Email</InputLabel>
                     <Input
                     id="email"
@@ -85,11 +112,11 @@ const Login = ({ className }: LoginProps) => {
                     value={user.email}
                     sx={{color: "initial"}}
                     onChange={(e) => setUser({...user, email: e.target.value})}
-                    onFocus={() => {setEmailError(false); setPwdError(false); setUniError(false)}}
+                    onFocus={() => {setEmailError(false); setPwdError(false); setUniError({status: false, msg: ''})}}
                     />
                     <FormHelperText id="fullname-text" hidden={!emailError}>{user.email.trim() == "" ? "Email field is required" : "Invalid email format: example@test.com"}</FormHelperText>
                 </FormControl>
-                <FormControl variant="standard" error={pwdError || uniError} fullWidth>
+                <FormControl variant="standard" error={pwdError || uniError.status} fullWidth>
                     <InputLabel htmlFor="password" sx={{color: "initial !important"}}>Password</InputLabel>
                     <Input
                     id="password"
@@ -98,7 +125,7 @@ const Login = ({ className }: LoginProps) => {
                     value={user.password}
                     sx={{color: "initial"}}
                     onChange={(e) => setUser({...user, password: e.target.value})}
-                    onFocus={() => {setEmailError(false); setPwdError(false); setUniError(false)}}
+                    onFocus={() => {setEmailError(false); setPwdError(false); setUniError({status: false, msg: ''})}}
                     />
                     {!pwdVisibility ? <VisibilityOutlined sx={{cursor: "pointer", position: "absolute", right: "15px", top: "30px", transform: "translate(-50%, -50%)"}} onClick={() => setPwdVisibility(!pwdVisibility)}/> : <VisibilityOffOutlined sx={{cursor: "pointer", position: "absolute", right: "15px", top: "30px", transform: "translate(-50%, -50%)"}} onClick={() => setPwdVisibility(!pwdVisibility)}/>}
                     <FormHelperText id="password-text" hidden={!pwdError}>Password field is required</FormHelperText>
