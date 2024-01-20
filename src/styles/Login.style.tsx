eff5f21006b6ad2f2799151ded3a7b176c7a100d
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-01-20 20:58:29 +0100

    Worked on .or

--- src/styles/Login.style.tsx
+++ src/styles/Login.style.tsx
@@ -83,6 +83,7 @@ const LoginStyle = styled(Login)`
                 left: 50%;
                 top: 50%;
                 transform: translate(-50%, -50%);
+                padding: 0 1em;
                 background-color: ${props => !props.dark ? "white" : "black"};
             }
         }
--- src/styles/Signup.style.tsx
+++ src/styles/Signup.style.tsx
@@ -57,6 +57,7 @@ const SignupStyle = styled(Signup)`
                 left: 50%;
                 top: 50%;
                 transform: translate(-50%, -50%);
+                padding: 0 1em;
                 background-color: ${props => !props.dark ? "white" : "black"};
             }
         }
