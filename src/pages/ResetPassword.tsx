d69fe3f21126545f5015285b147f76a2b9f0c7d8
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-01-27 03:05:07 +0100

    work on reset-password logic

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,5 +1,5 @@
-import { Button, Card, Typography } from "@mui/material";
-import { MouseEvent, useState } from "react";
+import { Alert, Button, Card, Typography } from "@mui/material";
+import { MouseEvent, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { getAuth, sendPasswordResetEmail, confirmPasswordReset } from "firebase/auth";
 import { app } from "../firebaseConfig";
@@ -15,10 +15,41 @@ const ResetPassword = ({ className }: ResetPasswordProps) => {
     const auth = getAuth(app)
 
     const [email, setEmail] = useState("");
+    const [emailErr, setEmailErr] = useState({
+        status: false,
+        msg: ""
+    })
+    const [uniError, setUniError] = useState({
+        status: false,
+        msg: ""
+    })
+    const [loading, isLoading] = useState(false)
     const [codeSent, isCodeSent] = useState(false);
+
+    const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g
+
+    useEffect(() => {
+        localStorage.setItem("email", JSON.stringify(email))
+    }, [email])
 
     const codeMail = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
+        if(email.trim() == ""){
+            setEmailErr({
+                status: true,
+                msg: "Your email is required"
+            })
+            return
+        }else if(!emailReg.test(email)){
+            setEmailErr({
+                status: true,
+                msg: "Incorrect Email format: example@test.com"
+            })
+            return
+        }
+
+        isLoading(true)
+
         sendPasswordResetEmail(auth, email)
             .then((res) => {
                 console.log(res)
@@ -26,7 +57,14 @@ const ResetPassword = ({ className }: ResetPasswordProps) => {
             })
             .catch((error) => {
                 console.log(error.code)
-            });
+                setUniError({
+                    status: true,
+                    msg: "An error occured"
+                })
+            })
+            .finally(() => {
+                isLoading(false)
+            })
     };
 
     return (
@@ -38,13 +76,16 @@ const ResetPassword = ({ className }: ResetPasswordProps) => {
                 <Card
                     component="form"
                     className={`${className} update-pwd`}
-                ></Card>
+                >
+                    
+                </Card>
             ) : (
                 <Card component="form" className={`${className} send-link`}>
                     <div>
                         <Link to="/login"><ArrowBack/></Link>
                         <Typography variant="h4">Password reset</Typography>
                     </div>
+                    <Alert severity="error" variant="filled" sx={{display: `${uniError.status ? "block" : "none"}`}}>{uniError.msg}</Alert>
                     <Typography variant="body2" sx={{opacity: ".8"}}>A link will be sent to your email to reset your password</Typography>
                     <input
                         type="email"
@@ -52,11 +93,13 @@ const ResetPassword = ({ className }: ResetPasswordProps) => {
                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="E-mail Address"
                     />
+                    <p style={{color: "red"}} hidden={!emailErr.status}>{emailErr.msg}</p>
                     <Button
                         variant="contained"
                         sx={{ backgroundColor: "tranparent" }}
                         type="submit"
                         onClick={codeMail}
+                        disabled={loading}
                     >
                         Send Link
                     </Button>
