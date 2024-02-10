550537dab171ceb09e8171160de385238d6ebd37
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-02-10 20:41:32 +0100

    change routed url from quiz page

--- src/pages/ProtectedRoutes/ProtectedQuestionRoute.tsx
+++ src/pages/ProtectedRoutes/ProtectedQuestionRoute.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react"
 import { useAppSelector } from "../../store"
-import { Navigate} from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 
 const ProtectedQuestionRoute = ({ children }: {children: ReactNode}) => {
 
     const { success } = useAppSelector(state => state.category)
+    const { categoryId } = useParams()
 
     if(!success){
-        return <Navigate to={`/`}/>
+        return <Navigate to={`/category/${categoryId}`}/>
     }
     return children
 }
